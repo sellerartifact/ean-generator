ea4472ef8d2bb7dbc212edec2ac2675216f8a648
王珏 <wangjuesix@gmail.com>
2020-12-12 12:53:42 +0800

    prettier代码

--- src/ean-generator.ts
+++ src/ean-generator.ts
@@ -1,7 +1,7 @@
 interface createProps {
-  countryCode?:string | number
-  vendorEan?:string|number
-  size?:number
+  countryCode?: string | number
+  vendorEan?: string | number
+  size?: number
 }
 
 export default class EanGenerator {
@@ -10,43 +10,44 @@ export default class EanGenerator {
   constructor(countryCodeArr: string[]) {
     this.setCountryCode(countryCodeArr)
   }
-  public create(prop:createProps = {size: 1}): string{
+  public create(prop: createProps = { size: 1 }): string {
     return this._create(prop)
   }
-  public createMultiple(prop:createProps = {size: 1}): string[] {
-    let {size} = prop
-    let arr:string [] = new Array(size)
-    if(!size || Number(size) <= 0 || isNaN(Number(size))){
+  public createMultiple(prop: createProps = { size: 1 }): string[] {
+    let { size } = prop
+    let arr: string[] = new Array(size)
+    if (!size || Number(size) <= 0 || isNaN(Number(size))) {
       size = 1
     }
-    for(let i=0; i< size; i++){
-      arr[i]=this._create(prop)
+    for (let i = 0; i < size; i++) {
+      arr[i] = this._create(prop)
     }
     return arr
   }
 
-  private _create(prop: createProps){
-    let {countryCode, vendorEan} = prop
+  private _create(prop: createProps) {
+    let { countryCode, vendorEan } = prop
     if (this.countryCodeArr.length === 0) {
       throw new RangeError('please set countryCode before call create!')
     }
-    if(countryCode && countryCode.toString().length != 3){
+    if (countryCode && countryCode.toString().length != 3) {
       throw new RangeError('the length of countryCode must be 6')
     }
-    if(vendorEan && vendorEan.toString().length!=6){
+    if (vendorEan && vendorEan.toString().length != 6) {
       throw new RangeError('the length of vendorEan must be 6')
     }
-    if(!countryCode){
+    if (!countryCode) {
       countryCode = this.pickCountryCode()
     }
-    if(!vendorEan){
+    if (!vendorEan) {
       vendorEan = this.fillNumber(6, Math.floor(Math.random() * 100000))
     }
-    let eanCode = countryCode.toString()
-      + vendorEan.toString()
-      + this.fillNumber(3, Math.floor(Math.random() * 999))
-    let lastestNum  = this.computedEanLastNum(eanCode)
-    return eanCode+lastestNum
+    let eanCode =
+      countryCode.toString() +
+      vendorEan.toString() +
+      this.fillNumber(3, Math.floor(Math.random() * 999))
+    let lastestNum = this.computedEanLastNum(eanCode)
+    return eanCode + lastestNum
   }
 
   public isValid(num: string): boolean {
@@ -58,12 +59,12 @@ export default class EanGenerator {
     return lastNum === computedLastNum
   }
 
-  public setCountryCode(countryCodeArr: string[]){
-    if(countryCodeArr.length === 0){
-       throw new RangeError('the length of countryCodeArr must be >=1')
+  public setCountryCode(countryCodeArr: string[]) {
+    if (countryCodeArr.length === 0) {
+      throw new RangeError('the length of countryCodeArr must be >=1')
     }
-    for(let i=0; i< countryCodeArr.length; i++){
-      if(countryCodeArr[i].length != 3){
+    for (let i = 0; i < countryCodeArr.length; i++) {
+      if (countryCodeArr[i].length != 3) {
         throw new Error(`the length of ${countryCodeArr[i]} must be 3`)
       }
     }
@@ -77,15 +78,15 @@ export default class EanGenerator {
     let bOdd = true
     for (let i = 0; i < subNum.length; i++) {
       if (bOdd) {
-        odd += Number(subNum[i]);
+        odd += Number(subNum[i])
       } else {
-        even += Number(subNum[i]);
+        even += Number(subNum[i])
       }
       bOdd = !bOdd
     }
-    let sum: string | number = odd + even * 3;
-    sum = sum.toString();
-    let lastestNum = (10 - Number(sum[sum.length - 1])) % 10;
+    let sum: string | number = odd + even * 3
+    sum = sum.toString()
+    let lastestNum = (10 - Number(sum[sum.length - 1])) % 10
     return lastestNum.toString()
   }
 
@@ -95,12 +96,10 @@ export default class EanGenerator {
   }
 
   private fillNumber(len: number, num: number): string {
-    num = Math.floor(Number(num));
-    let sub = num.toString().length;
+    num = Math.floor(Number(num))
+    let sub = num.toString().length
     let randomNum = Math.floor(Math.random() * 10).toString()
-    let fillStr = new Array(Math.abs(len - sub)).fill(randomNum).join("")
+    let fillStr = new Array(Math.abs(len - sub)).fill(randomNum).join('')
     return fillStr.toString() + num.toString()
   }
-
-
 }
--- test/ean-generator.test.ts
+++ test/ean-generator.test.ts
@@ -1,58 +1,54 @@
-import EanGenerator from "../src/ean-generator"
+import EanGenerator from '../src/ean-generator'
 
 let ean = new EanGenerator(['931', '983'])
 
-
-describe('create multiple', ()=>{
-  it("return ean code is not empty", () => {
-    let arr = ean.createMultiple({size: 10})
+describe('create multiple', () => {
+  it('return ean code is not empty', () => {
+    let arr = ean.createMultiple({ size: 10 })
     expect(arr).toBeTruthy()
     expect(arr.length).toBe(10)
-    for(let i=0;i<arr.length;i++){
+    for (let i = 0; i < arr.length; i++) {
       expect(arr[i].length).toBe(13)
     }
   })
 })
 
-describe("create test", () => {
-  it("return ean code is not empty", () => {
+describe('create test', () => {
+  it('return ean code is not empty', () => {
     expect(ean.create()).toBeTruthy()
   })
 
-  it("return ean len is 13", () => {
-      let code = ean.create()
-      expect(code.length).toBe(13)
+  it('return ean len is 13', () => {
+    let code = ean.create()
+    expect(code.length).toBe(13)
   })
 
-  it('test ean isValid method', ()=>{
+  it('test ean isValid method', () => {
     let code = ean.create()
     let res = ean.isValid(code)
     expect(res).toBe(true)
   })
 
-  it('input countryCode', ()=>{
-    let code = ean.create({countryCode: "123", vendorEan: "456789"})
+  it('input countryCode', () => {
+    let code = ean.create({ countryCode: '123', vendorEan: '456789' })
     expect(code.slice(0, 3)).toBe('123')
     expect(code.length).toBe(13)
   })
 
-  it('check countryCode boundary error', ()=>{
-     expect(()=>ean.create({countryCode: "1"})).toThrow(RangeError)
+  it('check countryCode boundary error', () => {
+    expect(() => ean.create({ countryCode: '1' })).toThrow(RangeError)
   })
 
-  it('check vendorEan boundary error', ()=>{
-    expect(()=>ean.create({vendorEan: "132"})).toThrow(RangeError)
+  it('check vendorEan boundary error', () => {
+    expect(() => ean.create({ vendorEan: '132' })).toThrow(RangeError)
   })
-  it('check vendorEan boundary error', ()=>{
-    expect(()=>{
+  it('check vendorEan boundary error', () => {
+    expect(() => {
       new EanGenerator([]).create()
     }).toThrow(RangeError)
 
-    expect(()=>{
+    expect(() => {
       new EanGenerator(['12']).create()
     }).toThrow(Error)
   })
 })
-
-
-
